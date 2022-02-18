abe1988155e47223c08218127ee0e4690a7e88d2
Priyanshu Yadav <yadavpriyanshu757@gmail.com>
2022-02-18 22:46:50 +0530

    Test cases added

--- tests/test1.c
+++ tests/test1.c
@@ -1,3 +1,8 @@
 int main(){
     int a=5;
+    for(int i=0;i<10;i++){
+        i++;
+    }
+    while(i--);
+    return 0;
 }
--- tests/test5.c
+++ tests/test5.c
@@ -1,7 +1,20 @@
 // I/O checks
 int main(){
-    int x;
+    int x=0;
     x++;
     float f=34.56;
+    switch(x){    
+        case 0:    
+        f=0;  
+        break;    
+        case 1:    
+        f=10000;
+        break;    
+        case -1:    
+        f=-10000;
+        break;    
+        default:    
+        f=0;
+    }    
     return 0;
 }
